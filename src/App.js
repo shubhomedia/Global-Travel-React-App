e0f3d92fdde0a1b1c4ca172be274fb7df394b028
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-10 18:05:22 +0600

    Main Router Configured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+      <div>
+
+      </div>
+      <div className="bg-indigo-600 w-full h-screen"></div>
     </div>
   );
 }
